ce8bb9a5197d7b6335caaffe4692dd38a8de708f
JayakrishnasaiChintagumpula <146905031+JayakrishnasaiChintagumpula@users.noreply.github.com>
2024-03-04 23:03:53 -0500

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -104,7 +104,7 @@ def question3():
     answers["(b) SSE"] = "4*((a*a)+(b*b)+(c*c))"
 
     # type: a string that evaluates to a float
-    answers["(c) SSE"] = "4*(((R/2)^2)+(R^2))"
+    answers["(c) SSE"] = "5*(R^2)"
 
     return answers
 
